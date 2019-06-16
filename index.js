511335da738d987c3078cefc18f58d3842558eab
chikutaku <chikutaku.iphone.2017@gmail.com>
2019-06-16 19:05:06 +0900

    a

--- index.js
+++ index.js
@@ -32,8 +32,9 @@ io.on('connection', function(socket){
 
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
-    //io.emit('chat message', msg);
-    io.to(room).emit('chat message', msg);
+    io.emit('chat message', "all:" + msg);
+    var msgtoroom = room + ":" + msg;
+    io.to(room).emit('chat message', msgtoroom);
   });
 });
 
